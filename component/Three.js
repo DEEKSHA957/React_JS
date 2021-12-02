7b78e3b25f4ff052987487cd7b2af71117fdb0b4
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-02 14:58:34 +0530

    reactdemo

---
+++ component/Three.js
@@ -0,0 +1,17 @@
+import React from "react";
+class Three extends React.Component {
+  render() {
+     return (
+        <div>
+          <center><h1>  ---JUICE SHOP--- </h1>
+          <hr/>
+           <div><b>Item:</b> <em>Mango Juice</em></div>
+           <div><b>Amount:</b> <em>30.00</em></div>
+           <div><b>Spend Date:</b> <em>2021-12-2</em></div>
+           <div><b>Category:</b> <em>Food</em></div>
+           </center>
+           </div>
+     );
+  }
+}
+export default Three;
