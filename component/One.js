aeeb2ebe018ef9f30712580c9fbd57e37d5cf9b0
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-02 12:40:11 +0530

    reactdemo

---
+++ component/One.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class One extends React.Component{
+    render() {
+        return (
+         <>
+         <h1>Hello GM</h1>
+         <h1>Have a nice day</h1>
+         </> 
+            );
+      
+    }
+    
+}
+
+export default One;
