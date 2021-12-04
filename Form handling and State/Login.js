b7ced6ea2d9aa2dc5baeb02e7b2961cc6c847477
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-04 21:00:10 +0530

    reactdemo

---
+++ Form handling and State/Login.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+    state = {
+        email:"",
+        password:"",
+        mobilenumb:""
+    }
+    updateHandler = (event)=>{
+        this.setState({
+            [event.target.name] : event.target.value
+
+        })
+    }
+    submitHanlder = (event)=>{
+        event.preventDefault();
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <>
+    <form onSubmit={this.submitHanlder}><center>
+        <label>Email </label>
+        <input type="text" placeholder="Email Id" onChange={this.updateHandler} name="email"/> <br/><br/>
+
+        <label>Password </label>
+        <input type="text" placeholder="Please Enter Password Id" onChange={this.updateHandler} name="password" /> <br/><br/>
+        <label>Mobile Number </label>
+        <input type="text" placeholder="Please Enter mobile numb" onChange={this.updateHandler} name="mobilenumber" /> <br/><br/>
+
+        <input type="submit" value="Login..."/></center>
+        </form>  
+
+        <hr/>
+        <pre>{JSON.stringify(this.state)}</pre>  
+            </>
+        )
+    }
+}
+
+export default Login;
