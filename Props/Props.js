6688ab0edc969c6a14a326d929b2c2d3947892ca
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-05 12:43:51 +0530

    image

---
+++ Props/Props.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Props extends Component {
+  
+    render() {
+        return (
+            <div>
+              <center>
+                <h3>Collection of Cars</h3>
+                <h5>Ferrari</h5>
+                <hr/>
+                <img src="https://tse2.mm.bing.net/th?id=OIP.TQ-z5jgNzgQ8Uki2XwO8HwHaEo&pid=Api&P=0&w=273&h=170" alt="display image" /><br/>
+                <img src="https://tse2.mm.bing.net/th?id=OIP.Z2OyXA_iv90mQ3hKo2-rWAHaFj&pid=Api&P=0&w=216&h=162" alt="display image" />
+  
+            </center>
+            </div>
+        );
+    }
+}
+
+export default Props;
