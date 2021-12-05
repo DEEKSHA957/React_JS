da9fab7b629695ffad089756737b8d94127036df
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-05 20:58:07 +0530

    functional

---
+++ component/Four.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Four(){
+    return(<div>
+        <h2>Product Name:Iphone 11</h2>
+        <h3>Product Price:55000</h3>
+    </div>
+    );
+}
+export default Four;
---
+++ component/Four1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Four from "./component/Four";
+function App(){
+    return (
+      <div>
+        <h2>App Component....</h2>
+        <hr />
+        <Four/>
+        <Four/>
+        <Four/>
+      </div>
+    );
+  }
+export default App;
