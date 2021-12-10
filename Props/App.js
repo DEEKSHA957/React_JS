2c3e9286380f9d8c37ce0f442372de33b58e99c2
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-10 11:40:16 +0530

    commit

---
+++ Props/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import Employee from './component/Employee';
+/*import EmployeeList from './component/EmployeeList';*/
+class App extends Component{
+  render(){
+    return( <div>
+      <h1>Employee Details</h1>
+<hr />
+<Employee />
+{/* <EmployeeList /> */}
+    </div>
+    )
+  }
+}
+
+export default App;
