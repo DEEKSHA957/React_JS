a6e09f1ca8e55c48cf3dbf7f112eb5cba79aff5b
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-13 20:31:38 +0530

    commit

---
+++ Hook/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Click from "./component/Click";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Click/>
+      </div>
+    );
+  }
+}
+export default App;
