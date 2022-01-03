cdf0b95a85e123e3ef2f0c5965eb07ae75e36e18
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2022-01-03 22:24:14 +0530

    commit

---
+++ Props/Component/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import ParentComp from './ParentComp';
+  
+function App() {
+  return (
+    <div className="App">
+      <h1><center>-----------METHODS AS PROPS-------------</center></h1>
+      <ParentComp />
+  
+    </div>
+  );
+}
+ 
+export default App;
